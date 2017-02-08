22428b21430b8c90165078323918d1e34423ce63
Evgenii Seliavka <eseliavka@griddynamics.com>
2017-02-08 11:58:26 +0300

    Wrap each changeset with SYNC_DDL

--- src/main/java/liquibase/ext/metastore/configuration/HiveMetastoreConfiguration.java
+++ src/main/java/liquibase/ext/metastore/configuration/HiveMetastoreConfiguration.java
@@ -3,7 +3,8 @@ package liquibase.ext.metastore.configuration;
 import liquibase.configuration.AbstractConfigurationContainer;
 
 public class HiveMetastoreConfiguration extends AbstractConfigurationContainer {
-    public static final String LOCK = "lock";
+    private static final String LOCK = "lock";
+    private static final String SYNC_DDL = "syncDDL";
 
     public HiveMetastoreConfiguration() {
         super("liquibase");
@@ -11,6 +12,10 @@ public class HiveMetastoreConfiguration extends AbstractConfigurationContainer {
                 .setDescription("Should Liquibase lock database while executing")
                 .setDefaultValue(true)
                 .addAlias("lock");
+        getContainer().addProperty(SYNC_DDL, Boolean.class)
+                .setDescription("Wrap every statement with SYNC_DDL")
+                .setDefaultValue(true)
+                .addAlias("syncDDL");
     }
 
     public boolean getLock() {
@@ -19,6 +24,15 @@ public class HiveMetastoreConfiguration extends AbstractConfigurationContainer {
 
     public HiveMetastoreConfiguration setLock(boolean noLock) {
         getContainer().setValue(LOCK, noLock);
+        return this;
+    }
+
+    public boolean getSyncDDL() {
+        return getContainer().getValue(SYNC_DDL, Boolean.class);
+    }
+
+    public HiveMetastoreConfiguration setSyncDDL(boolean syncDdl) {
+        getContainer().setValue(SYNC_DDL, Boolean.class);
         return this;
     }
 }
--- src/main/java/liquibase/ext/metastore/impala/database/ImpalaDatabase.java
+++ src/main/java/liquibase/ext/metastore/impala/database/ImpalaDatabase.java
@@ -1,10 +1,19 @@
 package liquibase.ext.metastore.impala.database;
 
+import liquibase.configuration.LiquibaseConfiguration;
+import liquibase.exception.LiquibaseException;
+import liquibase.ext.metastore.configuration.HiveMetastoreConfiguration;
 import liquibase.ext.metastore.database.HiveMetastoreDatabase;
+import liquibase.ext.metastore.utils.UserSessionSettings;
+import liquibase.sql.visitor.SqlVisitor;
+import liquibase.statement.SqlStatement;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 public class ImpalaDatabase extends HiveMetastoreDatabase {
+    private static Boolean syncDdl = LiquibaseConfiguration.getInstance().getConfiguration(HiveMetastoreConfiguration.class).getSyncDDL();
 
     public ImpalaDatabase() {
         super("Impala", "jdbc:impala", "com.cloudera.impala.jdbc41.Driver");
@@ -54,6 +63,21 @@ public class ImpalaDatabase extends HiveMetastoreDatabase {
         String dbName = tokens[tokens.length - 1].split(";")[0];
         String schema = getSchemaDatabaseSpecific("SHOW DATABASES '" + dbName + "'");
         return schema == null ? "default" : schema;
+    }
+
+    @Override
+    public void execute(SqlStatement[] statements, List<SqlVisitor> sqlVisitors) throws LiquibaseException {
+        if (syncDdl) {
+            List<SqlStatement> sqlStatementList = new ArrayList<SqlStatement>();
+            sqlStatementList.add(UserSessionSettings.syncDdlStart());
+            sqlStatementList.addAll(Arrays.asList(statements));
+            sqlStatementList.add(UserSessionSettings.syncDdlStop());
+            SqlStatement[] wrappedStatements = new SqlStatement[sqlStatementList.size()];
+            sqlStatementList.toArray(wrappedStatements);
+            super.execute(wrappedStatements, sqlVisitors);
+        } else {
+            super.execute(statements, sqlVisitors);
+        }
     }
 }
 
